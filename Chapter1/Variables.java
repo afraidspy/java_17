05aadd37c3979904755552afb099c66bf86bb01d
Jessica <jessica@gmail.com>
2024-06-16 19:50:54 -0600

    Adding codes related to Chapter1

---
+++ Chapter1/Variables.java
@@ -0,0 +1,20 @@
+
+public class Variables {
+
+    public static void main (String params []){
+
+        if(params.length > 0)
+            System.out.println("First exam: " + params[0]);
+        else 
+            System.out.println("No parameters");
+
+        // error: 'var' is not allowed in a compound declaration
+        //var name = "Jessica", lastName = "López";
+        var name = "Jessica";
+        var lastName = "López";
+
+        System.out.println("Names is " + name);
+
+    }
+
+}
