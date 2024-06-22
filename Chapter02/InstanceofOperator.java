699054979614b34103f427d07ce18443d3a81a5c
Jessica San <afraidspy@gmail.com>
2024-06-21 18:05:28 -0600

    Adding examples to work with  bitwise operator

--- Chapter02/InstanceofOperator.java
+++ Chapter02/InstanceofOperator.java
@@ -4,23 +4,35 @@ public class InstanceofOperator {
 
         Object obj = null;
         //If obj ==  null, always will be false the next condition 
+        //System.out.println("Class: " + obj.getClass()); => java.lang.NullPointerException:
         if (obj instanceof Object) {
             System.out.println("This is an instance of Object");
-
         } else {
             System.out.println("This is NOT an instance of Object");
         }
 
-        Object objStringObject = new String("Hola");
+        Object objStringObject = new String("Hello");
 
         if (objStringObject instanceof String) {
             //Casting to String
             String str = (String) objStringObject;
             // Work with object wich is an String
-            System.out.println("Longitud de la cadena: " + str.length());
+            System.out.println("Lenght: " + str.length());
         }
 
+        System.out.println("Class: " + objStringObject.getClass());//Return:  class java.lang.String
 
+        short height = 1, weight = 3;
+        short zabra = (short) ((byte) weight * (byte) height);
+
+        int a = 10; // 1010
+        int b = ~a; // 11111111111111111111111111110101
+
+        System.out.println("a: "+ a);
+        System.out.println("Bin a: " + Integer.toBinaryString(a));
+        System.out.println("b: "+ b);
+        System.out.println("Bin b: "+Integer.toBinaryString(b));
+        
     }
     
 }
