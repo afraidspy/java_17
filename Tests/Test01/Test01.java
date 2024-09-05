40cf11420101deab74308028761989765b14d972
Jessica <jessica@gmail.com>
2024-09-04 21:32:04 -0600

    Adding examples of tests

---
+++ Tests/Test01/Test01.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        int[] values = { 10, 30, 50 };
+        for( var val : values ){
+            var x = 0;
+            while(x<values.length){
+                 System.out.println(x+" "+val);
+                  x++;
+             }
+        }              
+
+        System.out.println("12345".charAt(6));
+
+        ArrayList list = new ArrayList<>();
+
+        int array[] = new int[0];
+     }
+
+     
+    
+}
